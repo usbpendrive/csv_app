949bf92eb77a5a8f39fd295a066ae8bcc8d0a2bf
usbpendrive <ferry.fernando@gmail.com>
2025-12-05 15:29:32 +0700

    Basic save to CSV app

--- index.js
+++ index.js
@@ -1,10 +1,50 @@
-import { writeFileSync } from 'fs';
+import { appendFileSync } from 'fs';
+import { createInterface } from "readline";
 
-const content = "Test Content";
 
-try {
-    writeFileSync("./test.txt", content);
-    console.log("Successfully written");
-} catch (error) {
-    console.log(error);
+const readline = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const readLineAsync = (message) =>
+    new Promise((resolve) => readline.question(message, resolve));
+
+class Person {
+    constructor(name = "",  number = "", email = "") {
+        this.name = name;
+        this.number = number;
+        this.email = email;
+    }
+
+    saveToCSV() {
+        const content = `${this.name}, ${this.number}, ${this.email}\n`;
+        try {
+            appendFileSync("./contact.csv", content);
+            console.log(`${this.name} saved!`);
+        } catch (err) {
+            console.log(err);
+        }
+    }
 }
+
+
+const startApp = async () => {
+    let shouldContinue = true;
+    while (shouldContinue) {
+        const name = await readLineAsync("Contact name: ");
+        const number = await readLineAsync("Contact number: ");
+        const email = await readLineAsync("Contact email: ");
+
+        const person = new Person(name, number, email);
+        person.saveToCSV();
+
+        const response = await readLineAsync("Continue? [y to continue]: ");
+        shouldContinue = response.toLowerCase() === "y";
+    }
+
+
+    readline.close();
+}
+
+startApp();
